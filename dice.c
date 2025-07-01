521a19c721972180b9efec122f248ff45f88f23d
Telun2 <ayukanyanman@gmail.com>
2025-07-01 18:40:02 +0900

    Greetings added

--- dice.c
+++ dice.c
@@ -7,9 +7,14 @@ int main(){
 
     int n=0,m=0;
     int total;
+    char name[100];
 
     n = (rand()%6)+1;
     m = (rand()%6)+1;
+
+    printf("What is your name?\n");
+    scanf("%s",name);
+    printf("Hello, %s\n",name);
 
     printf("Rolling dice...\n");
     printf("Die 1:%d\n",n);
